4268f3f1e284d5383b3634a0542d33dd5abf7d70
Kamran-Asghar <Kamran.techling@gmail.com>
2022-12-25 20:29:14 +0500

    auth file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization')
-        const decoded = jwt.verify(token, 'secret')
+        const decoded = jwt.verify(token, process.env.PRIVATE_KEY)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
         
          
